a84751f37f571b947ab843ca8a04242f89f9181b
Patrick Meaney <patrick.wm.meaney@gmail.com>
2024-07-15 12:12:11 -0500

    added comment on req-settings.js file -- not going to use it, gotta just finish the main aspect of the project

--- components/flight-ads-app/routesLibrary/req-settings.js
+++ components/flight-ads-app/routesLibrary/req-settings.js
@@ -1,3 +1,5 @@
+// Added this file... but not using it... it's just an example of an improvement
+// to DRY the code up a bit
 import { HDB_AUTH_TOKEN } from "../env.js";
 
 export const hdbReqHeaders = {
